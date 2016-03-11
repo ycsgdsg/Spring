0254228d3ea35454cb8c8b531500bb3d8472db41
ycsgdsg <ycsgdsg@163.com>
2016-03-11 19:12:33 +0800

    Add Servlet

---
+++ Identity/src/main/java/handler/RegisterHandler.java
@@ -0,0 +1,23 @@
+package handler;
+
+import org.springframework.web.HttpRequestHandler;
+import service.identity.userAccount.UserAccountService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by chaoyuan on 03/11/2016.
+ */
+public class RegisterHandler implements HttpRequestHandler {private UserAccountService userAccountService;
+
+    public void setUserAccountService(UserAccountService userAccountService) {
+        this.userAccountService = userAccountService;
+    }
+
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ Identity/src/test/java/spring/TestApplicationContext.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by chaoyuan on 03/11/2016.
+ */
+public class TestApplicationContext {
+
+    @Test
+    public void testApplicationContext() {
+        ApplicationContextHelper.instance().init();
+    }
+}
