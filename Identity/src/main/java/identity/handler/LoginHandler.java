d9f61b026b12287313d2d234d9756271538f5c70
ycsgdsg <ycsgdsg@163.com>
2016-04-24 22:49:10 +0800

    log4j

--- Identity/src/main/java/identity/handler/LoginHandler.java
+++ Identity/src/main/java/identity/handler/LoginHandler.java
@@ -1,9 +1,11 @@
 package identity.handler;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import database.service.identity.userAccount.UserAccountService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -13,11 +15,15 @@ import javax.annotation.Resource;
 @Controller
 public class LoginHandler {
 
+    Logger logger = Logger.getLogger(LoginHandler.class);
+
     @Resource(name = "userAccountService")
     private UserAccountService userAccountService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseBody
     public void createUser() {
+        logger.info("user logged in");
         System.out.println("123");
     }
 
--- Identity/src/main/java/identity/handler/RegisterHandler.java
+++ Identity/src/main/java/identity/handler/RegisterHandler.java
@@ -1,10 +1,12 @@
 package identity.handler;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import database.service.identity.userAccount.UserAccountService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -14,11 +16,15 @@ import javax.annotation.Resource;
 @Controller
 public class RegisterHandler {
 
+    private Logger logger = Logger.getLogger(RegisterHandler.class);
+
     @Resource(name = "userAccountService")
     private UserAccountService userAccountService;
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @ResponseBody
     public void createUser() {
-        System.out.println("123");
+        logger.info("user registered");
+        System.out.println("456");
     }
 }
